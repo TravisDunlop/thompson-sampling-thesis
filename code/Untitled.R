f823057b05d63c91289d3835463411c2059c3bd4
TravisDunlop <dunloptravis@gmail.com>
2018-06-30 18:46:26 +0200

    adding game theory, dynamic programming, and evolutionary results along with some writing

--- code/Untitled.R
+++ code/Untitled.R
@@ -10,13 +10,57 @@ results <- read.csv(paste0(folder, 'data/game_theory_results.csv'))
 
 head(results)
 
+results$loss_per_turn <- results$loss_1 / results$num_turns
+
 levels(results$payoff_type) <- c('identity', 'rock paper scissors', 'uniform')
 
-by_payoff <- results[results$num_actions %in% c(2, 15, 29) & results$regret_1 > 1, ]
+max_regret = data.table(results)[, list(regret=max(regret_1)), by = c('num_turns', 'num_actions', 'agent_1', 'payoff_type')]
+max_regret
+########################################################################
 
-by_payoff_dt <- data.table(by_payoff)
+by_turns <- max_regret[max_regret$num_actions %in% c(2, 15, 29) & max_regret$regret > 1, ]
 
-curve_fit = by_payoff_dt[, list(max=max(regret_1)), by = c('num_turns', 'agent_1', 'payoff_type', 'num_actions')]
+by_turns$num_actions <- as.factor(by_turns$num_actions)
+
+levels(by_turns$num_actions) <- c('N = 2', 'N = 15', 'N = 29')
+
+by_turns$constant =  by_turns$regret / sqrt(by_turns$num_turns)
+constants = data.table(by_turns)[, list(max=max(constant)), by = c('agent_1', 'payoff_type', 'num_actions')]
+constants
+
+x = seq(10, 1000)
+curves = merge(data.frame(constants), data.frame(x = x))
+curves$y = curves$max * sqrt(curves$x)
+
+head(curves)
+
+head(curve_fit)
+
+ggplot(by_turns, aes(num_turns, regret, col = agent_1)) + 
+  geom_point(alpha = 0.75) + 
+  geom_line(data = curves, aes(x, y)) +
+  scale_x_log10() + scale_y_log10() + facet_grid(payoff_type ~ num_actions) + 
+  labs(x = 'log number of turns (T)', y = 'maximum log total regret', col = 'policy',
+       title = 'Constant-Sum Game Results - analysis of maximum regret')
+
+########################################################################
+
+test <- data.frame(x = seq(2, 1000))
+test$logx = log(test$x)
+test$sqrtx = 5 * sqrt(test$x)
+
+ggplot(test, aes(x, sqrtx)) + geom_line() + scale_x_log10() + scale_y_log10()
+
+########################################################################
+
+unique(results$num_turns)
+
+by_actions <- max_regret[max_regret$num_turns %in% c(10, 100, 1000) & max_regret$regret > 1, ]
+
+by_actions$num_turns <- as.factor(by_actions$num_turns)
+
+levels(by_actions$num_turns) <- c('T = 10', 'T = 100', 'T = 1000')
+
 curve_fit$constant =  curve_fit$max / sqrt(curve_fit$num_turns)
 constants = data.table(curve_fit)[, list(max=max(constant)), by = c('agent_1', 'payoff_type', 'num_actions')]
 constants
@@ -29,17 +73,11 @@ curves
 
 head(curve_fit)
 
-ggplot(curve_fit, aes(num_turns, max, col = agent_1)) + 
+ggplot(by_actions, aes(num_actions, regret, col = agent_1)) + 
   geom_point(alpha = 0.75) + 
   #geom_point(data = by_payoff, aes(num_turns, regret_1), alpha = 0.01) +
-  geom_line(data = curves, aes(x, y)) +
-  scale_x_log10() + scale_y_log10() + facet_grid(payoff_type ~ num_actions) + 
-  labs(x = 'log number of turns', y = 'log total regret', col = 'policy')
+  #geom_line(data = curves, aes(x, y)) +
+  scale_y_log10() + facet_grid(payoff_type ~ num_turns) + 
+  labs(x = 'number of actions (N)', y = 'maximum log total regret', col = 'policy', 
+      title = 'Constant-Sum Game Results - analysis of maximum regret')
 
-
-test <- data.frame(x = seq(2, 1000))
-test$logx = log(test$x)
-test$sqrtx = 5 * sqrt(test$x)
-
-
-ggplot(test, aes(x, sqrtx)) + geom_line() + scale_x_log10() + scale_y_log10()
